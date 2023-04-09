e387b5df17967dc42632b87ff91bdba51300396e
cononco99 <cononco99@gmail.com>
2023-04-09 16:27:02 -0700

    moved most main functionality from main.rs to libmain.rs to make testing easier.

---
+++ src/libmain.rs
@@ -0,0 +1,35 @@
+use structopt::StructOpt;
+use std::ffi::OsString;
+
+use crate::schwab_transaction::SchwabTransaction;
+use stable_eyre::eyre::*;
+use crate::file_names::FileNames;
+use crate::opt::Opt;   
+
+pub fn libmain<I>(iter: I) -> Result<()>
+    where
+                I: IntoIterator,
+                    I::Item: Into<OsString> + Clone, 
+    {
+
+    let opts = Opt::from_iter(iter);
+    let file_names = FileNames::new(&opts)?;
+
+    let transactions_csv = SchwabTransaction::read_transactions_csv(&opts.transactions)
+        .with_context(|| {
+            format!(
+                "unable to read transactions .CSV file : {:#?}",
+                &opts.transactions
+            )
+        })?;
+
+    let transactions =
+        SchwabTransaction::to_transactions(&transactions_csv, &opts.current_securities)
+            .with_context(|| "unable to create qif Transactions. ".to_string())?;
+
+    transactions
+        .print_qifs(&file_names, &opts.linked_acct)
+        .with_context(|| "unable to create qif files. ".to_string())?;
+
+    Ok(())
+}
